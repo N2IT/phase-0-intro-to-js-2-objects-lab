b89bd47761951fc07cfaec7b8f940bde7960c9c1
Tony Eder <tonyeder11@gmail.com>
2023-10-15 17:29:52 -0700

    passed

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
 // Write your solution in this file!
 //Assign an employee variable to an Object containing name and streetAddress keys
+
 const employee = {
-    name: "Adam",
-    streetAddress: "1980 Internet",
+    name: "Tony",
+    streetAddress: "8856 34th Ave SW",
 }
 
-//Function should take in 3 arguments: employee object, a key, and a value.
-//Should not mutate the employee; should return a new object that has been updated value for the key passed in.
-function updateEmployeeWithKeyAndValue(obj, key, value) {
-    return {
-        ...obj,
-        [key]: value,
-    };
+function updateEmployeeWithKeyAndValue( obj, key, value) {
+    const newObject = {...obj};
+    newObject[key] = value;
+
+    return newObject;
 }
 
-const newEmployee = updateEmployeeWithKeyAndValue(
-    employee,
-    "name",
-    "Eve"
-);
-
-//this function should work the same as the prior but it should mutate the employee object
-function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value) {
     obj[key] = value;
+
     return obj;
 }
 
-//this function should take ina employee object and a key
-//it should delete the property with that key from the employee object. should not mutate the original employee object
-function deleteFromEmployeeByKey(obj, key, value) {
-    return {
-        ...obj,
-        [key]: value,
-    };
+function deleteFromEmployeeByKey (obj, key) {
+    const newObject = {...obj};
+    delete newObject[key];
+    return newObject;
 }
 
-//should work the same as prior. but it should mutate the employee object.
-function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
-    obj[key] = value;
+function destructivelyDeleteFromEmployeeByKey (obj, key) {
+    delete obj[key];
     return obj;
 }
